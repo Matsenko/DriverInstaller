c343f5a5f021d7a92fed414d19de738f1b1d1d8f
Matsenko <vovamatsenko89@gmail.com>
2024-03-25 13:48:10 +0200

    WinForms

---
+++ DriverInstallerWinForms/Program.cs
@@ -0,0 +1,27 @@
+using System.Security.Principal;
+
+namespace DriverInstallerWinForms
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            WindowsPrincipal principal = new WindowsPrincipal(identity);
+            bool isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            ApplicationConfiguration.Initialize();
+            if (isAdmin)
+            {
+                Application.Run(new Form1());
+            }
+            else
+            {
+                MessageBox.Show("Please run this application as an administrator.");
+            }
+        }
+    }
+}
